dfe41f91a848f50c77358fc898a8c655f03a2a91
euelisee <euelisee@gmail.com>
2025-10-18 17:42:52 -0300

    Base do projeto

---
+++ src/main/java/com/fiap/finbal/FinbalApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.finbal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinbalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/finbal/FinbalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.finbal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinbalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
