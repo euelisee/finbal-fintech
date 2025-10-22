7871b8e87877fc2c2e9649b538b465ad7808f8f9
euelisee <euelisee@gmail.com>
2025-10-22 12:58:38 -0300

    Ajustes adicionais

--- src/main/java/com/fiap/finbal/Controller/UsuarioController.java
+++ src/main/java/com/fiap/finbal/Controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin; 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.fiap.finbal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") 
 public class UsuarioController {
 
     private final UsuarioService service;
@@ -77,10 +79,10 @@ public class UsuarioController {
         } catch (RuntimeException e) {
             MensagemResponseDTO erro = new MensagemResponseDTO(
                     HttpStatus.NOT_FOUND,
-                    e.getMessage(),
+                    e.getMessage(), // <-- Linha que estava faltando o restante do código
                     null);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
         }
     }
 
-}
+} 
