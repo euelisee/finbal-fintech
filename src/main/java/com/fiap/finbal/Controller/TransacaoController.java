cc70e96edb02eaa91dffdf72bebb29d6f17f209e
Gabrielli-Martinelli <gabriellimartinelli@outlook.com>
2025-11-04 11:24:43 -0300

    FEAT: delecao e atualizacao classe transacao

--- src/main/java/com/fiap/finbal/Controller/TransacaoController.java
+++ src/main/java/com/fiap/finbal/Controller/TransacaoController.java
@@ -1,6 +1,7 @@
 package com.fiap.finbal.controller;
 
 import com.fiap.finbal.DTO.TransacaoRequestDTO;
+import com.fiap.finbal.Model.Usuario;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,6 +44,38 @@ public class TransacaoController {
                     null);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
         }
+    }
+
+    @DeleteMapping("/transacoes/{id}")
+    public ResponseEntity<MensagemResponseDTO> deletarTransacao(@PathVariable Long id) {
+        try {
+            service.deletarTransacao(id);
+            MensagemResponseDTO resposta = new MensagemResponseDTO(
+                    HttpStatus.OK,
+                    "Transação deletada com sucesso!",
+                    null);
+            return ResponseEntity.status(HttpStatus.OK).body(resposta);
+        } catch (RuntimeException e) {
+            MensagemResponseDTO erro = new MensagemResponseDTO(
+                    HttpStatus.NOT_FOUND,
+                    e.getMessage(),
+                    null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+        }
+    }
+
+    @PutMapping("/transacoes/{id}")
+    public ResponseEntity<MensagemResponseDTO> atualizarTransacaoPorId(
+            @PathVariable Long id,
+            @RequestBody TransacaoRequestDTO transacaoAtualizadaDTO) {
+
+        Transacao transacao = service.atualizarTransacao(id, transacaoAtualizadaDTO);
+        MensagemResponseDTO resposta = new MensagemResponseDTO(
+                HttpStatus.OK,
+                "Transação atualizada com sucesso!",
+                transacao);
+
+        return ResponseEntity.status(HttpStatus.OK).body(resposta);
     }
 
 
--- src/main/java/com/fiap/finbal/Service/TransacaoService.java
+++ src/main/java/com/fiap/finbal/Service/TransacaoService.java
@@ -52,7 +52,7 @@ public class TransacaoService {
         return transacaoRepository.findAll();
     }
 
-    public Optional<Transacao> buscarPorId(Long id) {
+    public Optional<Transacao> buscarTransacaoPorId(Long id) {
         return transacaoRepository.findById(id);
     }
 
@@ -91,4 +91,72 @@ public class TransacaoService {
         return transacaoRepository.save(novaTransacao);
     }
 
+    @Transactional
+    public void deletarTransacao(Long id) {
+        Transacao transacaoExistente = this.buscarTransacaoPorId(id)
+                .orElseThrow(() -> new RuntimeException("Transação não encontrada para deleção (ID: " + id + ")"));
+
+        Conta conta = transacaoExistente.getConta();
+
+        switch (transacaoExistente.getTipo()) {
+            case RECEITA:
+                conta.setSaldo(conta.getSaldo().subtract(transacaoExistente.getValor()));
+                transacaoRepository.deleteById(id);
+                break;
+            case DESPESA:
+                conta.setSaldo(conta.getSaldo().add(transacaoExistente.getValor()));
+                transacaoRepository.deleteById(id);
+                break;
+            default:
+                throw new RuntimeException("ERRO: Tipo de transação desconhecido.");
+        }
+        contaRepository.save(conta);
+    }
+
+    @Transactional
+    public Transacao atualizarTransacao(Long id, TransacaoRequestDTO dto) {
+        return transacaoRepository.findById(id)
+                .map(transacaoExistente -> {
+                    Conta conta = transacaoExistente.getConta();
+
+                    switch (transacaoExistente.getTipo()) {
+                        case RECEITA:
+                            conta.setSaldo(conta.getSaldo().subtract(transacaoExistente.getValor()));
+                            break;
+                            case DESPESA:
+                                conta.setSaldo(conta.getSaldo().add(transacaoExistente.getValor()));
+                                break;
+                    }
+
+                    if (dto.tipo() != null) {
+                        transacaoExistente.setTipo(dto.tipo());
+                    }
+                    if (dto.valor() != null) {
+                        transacaoExistente.setValor(dto.valor());
+                    }
+                    if (dto.categoria() != null) {
+                        transacaoExistente.setCategoria(dto.categoria());
+                    }
+                    if (dto.data() != null) {
+                        transacaoExistente.setData(dto.data());
+                    }
+
+                    switch (transacaoExistente.getTipo()) {
+                        case RECEITA:
+                            conta.setSaldo(conta.getSaldo().add(transacaoExistente.getValor()));
+                            break;
+                        case DESPESA:
+                            if (conta.getSaldo().compareTo(transacaoExistente.getValor()) < 0) {
+                                throw new RuntimeException("Saldo insuficiente para atualizar esta despesa.");
+                            }
+                            conta.setSaldo(conta.getSaldo().subtract(transacaoExistente.getValor()));
+                            break;
+                    }
+
+                    contaRepository.save(conta);
+                    return transacaoRepository.save(transacaoExistente);
+                })
+
+                .orElseThrow(() -> new RuntimeException("Transação não encontrada para atualização (ID: " + id + ")"));
+    }
 }
