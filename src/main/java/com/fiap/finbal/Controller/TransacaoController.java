4691f70bb176b1e13a8dc9076891e751ebc00105
Gabrielli Martinelli <gabriellimartinelli@outlook.com>
2025-11-06 00:38:50 -0300

    Add again CrossOrigin annotation to TransacaoController

--- src/main/java/com/fiap/finbal/Controller/TransacaoController.java
+++ src/main/java/com/fiap/finbal/Controller/TransacaoController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransacaoController {
 
     private final TransacaoService service;
